ad636defcb815fa72ec1c01a19e8ae7bad49297f
momooo3 <130732458+momooo3@users.noreply.github.com>
2023-06-02 16:36:54 +0800

    6.2-7
    6.2-7

--- main.js
+++ main.js
@@ -3,42 +3,42 @@ console.log("My JS file is loading successfully!");
 let enterEventCount = 0;
 let leaveEventCount = 0;
 const body = document.getElementById('body');
-const img18 = document.getElementById('img18');
-const img19 = document.getElementById('img19');
-const img110 = document.getElementById('img110');
+const hercake = document.getElementById('hercake');
+const day = document.getElementById('day');
+const whenan = document.getElementById('whenan');
 
 
-img18.addEventListener('mouseenter', (e) => {
+hercake.addEventListener('mouseenter', (e) => {
   body.style.background = 'rgb(0, 200, 255)';
   enterEventCount++;
   
 });
 
-img18.addEventListener('mouseleave', (e) => {
+hercake.addEventListener('mouseleave', (e) => {
     body.style.background = 'white';
   leaveEventCount++;
   
 });
 
-img19.addEventListener('mouseenter', (e) => {
+day.addEventListener('mouseenter', (e) => {
     body.style.background = 'rgb(37, 224, 20)';
     enterEventCount++;
     
   });
   
-  img19.addEventListener('mouseleave', (e) => {
+  day.addEventListener('mouseleave', (e) => {
       body.style.background = 'white';
     leaveEventCount++;
     
   });
 
-  img110.addEventListener('mouseenter', (e) => {
+  whenan.addEventListener('mouseenter', (e) => {
     body.style.background = 'rgb(235, 235, 11)';
     enterEventCount++;
     
   });
   
-  img110.addEventListener('mouseleave', (e) => {
+  whenan.addEventListener('mouseleave', (e) => {
       body.style.background = 'white';
     leaveEventCount++;
     
